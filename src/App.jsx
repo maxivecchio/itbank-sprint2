8da27fea204565017622a472b989f414991d425c
Maxi Vecchio <maximilianovecchio@gmail.com>
2024-09-04 12:23:26 -0300

    Graficos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Component as PieChart } from "./components/shared/pie-chart";
+
 function App() {
   return (
-    <>
-      <div className="">asdasd</div>
-    </>
+    <div className="p-12">
+      <PieChart />
+    </div>
   );
 }
 
