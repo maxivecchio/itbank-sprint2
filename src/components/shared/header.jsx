7312e2bb1cce570663da3e506cb960855edc62bd
Maxi Vecchio <maximilianovecchio@gmail.com>
2024-09-05 09:32:34 -0300

    Header toggle sidebar

--- src/components/shared/header.jsx
+++ src/components/shared/header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 
-
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,13 +11,20 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { buttonVariants } from "@/components/ui/button";
-const Header = () => {
+import { Button, buttonVariants } from "@/components/ui/button";
+const Header = ({ sidebarOpen, setSidebarOpen, toggleSidebar }) => {
   return (
     <header className="fixed w-full z-50 p-3 bg-[#1a1a1a] h-14 flex items-center justify-between	 text-white">
       <div className="flex items-center">
+        <Button
+          variant="ghost"
+          className="bg-transparent md:hidden"
+          onClick={toggleSidebar}
+        >
+          a
+        </Button>
         <img className="h-7" src="/logo.webp" alt="Rossum Logo" />
-        <span className="font-bold ml-2 text-lg">
+        <span className="font-bold ml-2 text-lg hidden md:inline-block">
           <span className="logo_name">Rossum | </span>HomeBanking
         </span>
       </div>
@@ -28,10 +34,10 @@ const Header = () => {
             variant: "outline",
           })} bg-transparent`}
         >
-          Juan R Gomez
+          <FaUser /> Juan R Gomez
         </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel ><FaUser></FaUser> Mi cuenta </DropdownMenuLabel>
+          <DropdownMenuLabel> Mi cuenta </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem> Ver Perfil</DropdownMenuItem>
           <DropdownMenuItem>Cerrar Sesión</DropdownMenuItem>
