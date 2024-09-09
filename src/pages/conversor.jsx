464ae9b7a258b8c1814da9b2b1521de045c7cabf
Maxi Vecchio <maximilianovecchio@gmail.com>
2024-09-09 12:05:39 -0300

    Conversor

--- src/pages/conversor.jsx
+++ src/pages/conversor.jsx
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-const currencies = ["USD", "EUR", "ARS", "JPY", "GBP"];
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const CurrencyConverter = () => {
-  const [inputCurrency, setInputCurrency] = useState("ARS");
-  const [outputCurrency, setOutputCurrency] = useState("USD");
-  const [amount, setAmount] = useState(1);
-  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [amountARS, setAmountARS] = useState(0);
+  const [amountUSD, setAmountUSD] = useState(0);
   const [dolarType, setDolarType] = useState("");
   const [error, setError] = useState("");
   const [dolarOptions, setDolarOptions] = useState([]);
+  const [userHasInput, setUserHasInput] = useState(false);
 
   useEffect(() => {
-    // Obtener las tasas de dólar al cargar el componente
     axios
       .get("https://dolarapi.com/v1/dolares")
       .then((response) => {
         const data = response.data;
         setDolarOptions(data);
-        setDolarType(data[0]?.casa || ""); // Selecciona el primer tipo de dólar por defecto
+        setDolarType(data[0]?.casa || "");
       })
       .catch((err) => {
         setError("Error al obtener las tasas de dólar");
@@ -27,83 +30,134 @@ const CurrencyConverter = () => {
       });
   }, []);
 
-  const convertCurrency = () => {
-    if (amount === "" || isNaN(amount) || amount <= 0) {
+  const handleConversion = (
+    value,
+    fromCurrency,
+    toCurrency,
+    setFromCurrency,
+    setToCurrency,
+    rate
+  ) => {
+    setFromCurrency(value);
+    setUserHasInput(true);
+
+    if (value === "" || isNaN(value) || value <= 0) {
       setError("Por favor, ingrese un número válido");
-      setConvertedAmount(0);
+      setToCurrency(0);
       return;
     }
 
     setError("");
 
-    const selectedRate = dolarOptions.find((option) => option.casa === dolarType);
-    if (selectedRate) {
-      const rate = selectedRate.venta;
-      const result = amount / rate;
-      setConvertedAmount(result.toFixed(3));
+    if (rate) {
+      const result = fromCurrency === "ARS" ? value / rate : value * rate;
+      setToCurrency(result.toFixed(3));
     } else {
       setError("Tipo de dólar no disponible");
     }
   };
 
+  const handleARSChange = (e) => {
+    const value = e.target.value;
+    const selectedRate = dolarOptions.find(
+      (option) => option.casa === dolarType
+    )?.venta;
+
+    handleConversion(
+      value,
+      "ARS",
+      "USD",
+      setAmountARS,
+      setAmountUSD,
+      selectedRate
+    );
+  };
+
+  const handleUSDChange = (e) => {
+    const value = e.target.value;
+    const selectedRate = dolarOptions.find(
+      (option) => option.casa === dolarType
+    )?.venta;
+
+    handleConversion(
+      value,
+      "USD",
+      "ARS",
+      setAmountUSD,
+      setAmountARS,
+      selectedRate
+    );
+  };
+
+  useEffect(() => {
+    if (dolarType && userHasInput) {
+      const selectedRate = dolarOptions.find(
+        (option) => option.casa === dolarType
+      )?.venta;
+      handleConversion(
+        amountARS,
+        "ARS",
+        "USD",
+        setAmountARS,
+        setAmountUSD,
+        selectedRate
+      );
+    }
+  }, [dolarType]);
+
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-4 text-center">Currency Converter</h2>
+    <div className="max-w-md mt-10 p-6 bg-white rounded-lg">
+      <h2 className="text-2xl font-bold mb-4 text-center">
+        Conversor de monedas
+      </h2>
       {error && <p className="text-red-500 mb-4">{error}</p>}
       <div className="flex mb-4">
-        <input
-          type="number"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          className="w-full p-2 border border-gray-300 rounded mr-2"
-          placeholder="Amount"
-        />
-        <select
-          value={inputCurrency}
-          onChange={(e) => setInputCurrency(e.target.value)}
-          className="p-2 border border-gray-300 rounded"
-        >
-          {currencies.map((currency) => (
-            <option key={currency} value={currency}>
-              {currency}
-            </option>
-          ))}
-        </select>
+        <div className="border-2 flex w-full">
+          <input
+            type="number"
+            value={amountARS}
+            onChange={handleARSChange}
+            className="w-full p-2 border-gray-300 rounded mr-2"
+            placeholder="Cantidad en ARS"
+          />
+          <div className="flex items-center justify-center w-64 px-3 bg-gray-100 rounded-tl rounded-bl">
+            ARS
+          </div>
+        </div>
       </div>
       <div className="flex mb-4">
-        <select
-          value={dolarType}
-          onChange={(e) => setDolarType(e.target.value)}
-          className="p-2 border border-gray-300 rounded mr-2"
-        >
-          {dolarOptions.map((option) => (
-            <option key={option.casa} value={option.casa}>
-              {option.nombre}
-            </option>
-          ))}
-        </select>
-        <select
-          value={outputCurrency}
-          onChange={(e) => setOutputCurrency(e.target.value)}
-          className="p-2 border border-gray-300 rounded"
-        >
-          {currencies.map((currency) => (
-            <option key={currency} value={currency}>
-              {currency}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="text-center">
-        <p className="text-lg font-semibold">
-          Converted Amount: {convertedAmount} {outputCurrency}
-        </p>
-        <button
-          onClick={convertCurrency}
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-        >
-          Convertir
-        </button>
+        <div className="border-2 flex w-full">
+          <input
+            type="number"
+            value={amountUSD}
+            onChange={handleUSDChange}
+            className="w-full p-2"
+            placeholder="Cantidad en USD"
+          />
+          {dolarType && (
+            <Select
+              onValueChange={(value) => setDolarType(value)}
+              defaultValue={dolarType}
+            >
+              <SelectTrigger className="border-none w-64 px-3 bg-gray-100">
+                <SelectValue placeholder="Selecciona el tipo de dólar" />
+              </SelectTrigger>
+              <SelectContent>
+                {dolarOptions.map((option) => (
+                  <SelectItem
+                    className=""
+                    key={option.casa}
+                    value={option.casa}
+                  >
+                    {option.nombre === "Contado con liquidación"
+                      ? "Dólar Contado con liqui."
+                      : "Dólar " + option.nombre}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          )}
+        </div>
       </div>
     </div>
   );
