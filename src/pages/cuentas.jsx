2cbf2a2ea730e87f6c5f61c84579d70fd97a394f
Magali Plotek <magaliplotek@gmail.com>
2024-09-09 12:11:53 -0300

    Cambios cuentas y transferencias

--- src/pages/cuentas.jsx
+++ src/pages/cuentas.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import { Button, buttonVariants } from '@/components/ui/button'
 
 const Cuentas = () => {
   return (
-    <div>Cuentas</div>
+    <main className="container mx-auto p-4 bg-gray-100 rounded-lg">
+      <div className="dashboard max-w-[900px] mx-auto ml-0 text-left">
+        <div>
+          <h1 className="text-3xl font-bold mb-6">Mis Cuentas</h1>
+        </div>
+
+        <div className="accounts-section max-w-lg mx-auto p-4 ml-0">
+          <div className="account-card bg-white rounded-lg shadow-md mb-5 p-5">
+            <h2 className="text-lg font-semibold">Cuenta Corriente</h2>
+            <div className="saldotransacciones mb-4">
+              <p className="my-1">Saldo: $1,250</p>
+              <p className="my-1">Última transacción: $-50.00</p>
+            </div>
+            <Link className={buttonVariants()} to="/transferencias">
+               Ver Detalles de Transacciones
+            </Link>
+          </div>
+
+          <div className="account-card bg-white rounded-lg shadow-md mb-5 p-5">
+            <h2 className="text-lg font-semibold">Cuenta de Ahorros</h2>
+            <p className="my-1">Saldo: $5,789</p>
+            <p className="my-1">Última transacción: $+200.00</p>
+            <Link className={buttonVariants()} to="/transferencias">
+               Ver Detalles de Transacciones
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
